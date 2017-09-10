4576ed1cfa4af97fb3487cc0931c62168a6f873a
Tarmi <developer@clustertoken.io>
2017-09-10 17:29:13 +0200

    change path to equal git name

--- start-services.sh
+++ start-services.sh
@@ -13,18 +13,18 @@ sudo service rabbitmq-server start
 sudo service mongod start 
 
 #bridge
-pm2 start -n bridge $DIR/digipulse/node_modules/.bin/storj-bridge -- -c $DIR/digipulse/config/brige/config.json
-pm2 start -n monitor $DIR/digipulse/node_modules/.bin/storj-monitor -- -c $DIR/digipulse/config/brige/monitor.json
+pm2 start -n bridge $DIR/Digipulse-alpha/node_modules/.bin/storj-bridge -- -c $DIR/Digipulse-alpha/config/brige/config.json
+pm2 start -n monitor $DIR/Digipulse-alpha/node_modules/.bin/storj-monitor -- -c $DIR/Digipulse-alpha/config/brige/monitor.json
 
 #landlord
-pm2 start -n landlord $DIR/digipulse/node_modules/.bin/storj-complex -- -c $DIR/digipulse/config/complex/landlord.json
+pm2 start -n landlord $DIR/Digipulse-alpha/node_modules/.bin/storj-complex -- -c $DIR/Digipulse-alpha/config/complex/landlord.json
 
 #nodes
-pm2 start -n renter-1 $DIR/digipulse/node_modules/.bin/storj-complex -- -c $DIR/digipulse/config/complex/renter-1.json
-pm2 start -n renter-2 $DIR/digipulse/node_modules/.bin/storj-complex -- -c $DIR/digipulse/config/complex/renter-2.json
-pm2 start -n renter-3 $DIR/digipulse/node_modules/.bin/storj-complex -- -c $DIR/digipulse/config/complex/renter-3.json
-pm2 start -n renter-4 $DIR/digipulse/node_modules/.bin/storj-complex -- -c $DIR/digipulse/config/complex/renter-4.json
-pm2 start -n renter-5 $DIR/digipulse/node_modules/.bin/storj-complex -- -c $DIR/digipulse/config/complex/renter-5.json
-pm2 start -n renter-6 $DIR/digipulse/node_modules/.bin/storj-complex -- -c $DIR/digipulse/config/complex/renter-6.json
+pm2 start -n renter-1 $DIR/Digipulse-alpha/node_modules/.bin/storj-complex -- -c $DIR/Digipulse-alpha/config/complex/renter-1.json
+pm2 start -n renter-2 $DIR/Digipulse-alpha/node_modules/.bin/storj-complex -- -c $DIR/Digipulse-alpha/config/complex/renter-2.json
+pm2 start -n renter-3 $DIR/Digipulse-alpha/node_modules/.bin/storj-complex -- -c $DIR/Digipulse-alpha/config/complex/renter-3.json
+pm2 start -n renter-4 $DIR/Digipulse-alpha/node_modules/.bin/storj-complex -- -c $DIR/Digipulse-alpha/config/complex/renter-4.json
+pm2 start -n renter-5 $DIR/Digipulse-alpha/node_modules/.bin/storj-complex -- -c $DIR/Digipulse-alpha/config/complex/renter-5.json
+pm2 start -n renter-6 $DIR/Digipulse-alpha/node_modules/.bin/storj-complex -- -c $DIR/Digipulse-alpha/config/complex/renter-6.json
 
 
